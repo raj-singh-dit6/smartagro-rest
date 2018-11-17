dadcb86786d32fb173b8b95d8907e43d947faac5
Sagar Kanojia <sagar.k@mindfiresolutions.com>
2018-11-18 04:52:40 +0530

    Fix: Login response change

---
+++ src/main/java/gov/smartagro/api/controller/NotificationController.java
@@ -0,0 +1,8 @@
+package gov.smartagro.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+}
--- src/main/java/gov/smartagro/api/payload/JwtAuthenticationResponse.java
+++ src/main/java/gov/smartagro/api/payload/JwtAuthenticationResponse.java
@@ -1,11 +1,13 @@
 package gov.smartagro.api.payload;
 
+import gov.smartagro.api.model.User;
 import lombok.Data;
 
 @Data
 public class JwtAuthenticationResponse {
     private String accessToken;
     private String tokenType = "Bearer";
+    private User user;
 
     public JwtAuthenticationResponse(String accessToken) {
         this.accessToken = accessToken;
