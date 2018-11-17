e0155bfa34e6a22ae5ebeab191dad1ef8a78ff3a
rajendra.singh <rajendra.singh@mindfiresolutions.com>
2018-11-17 15:40:28 +0530

    @Data added

--- src/main/java/gov/smartagro/api/model/User.java
+++ src/main/java/gov/smartagro/api/model/User.java
@@ -1,16 +1,28 @@
 package gov.smartagro.api.model;
 
-import org.hibernate.annotations.NaturalId;
-
-import gov.smartagro.api.model.audit.DateAudit;
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.NaturalId;
+
+import gov.smartagro.api.model.audit.DateAudit;
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {
@@ -42,7 +54,16 @@ public class User extends DateAudit {
     @NotBlank
     @Size(max = 100)
     private String password;
-
+    
+    @Size(max = 100)
+    private String mobileNumber;
+    
+    @Size(max = 100)
+    private String soilHealthCardId;
+    
+    @Size(max = 100)
+    private String adharCardNo;
+    
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
@@ -60,51 +81,4 @@ public class User extends DateAudit {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
--- src/main/java/gov/smartagro/api/model/audit/DateAudit.java
+++ src/main/java/gov/smartagro/api/model/audit/DateAudit.java
@@ -1,15 +1,20 @@
 package gov.smartagro.api.model.audit;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.io.Serializable;
-import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Data;
+
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(
